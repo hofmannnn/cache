aa0059917efbeda8466bd1a6fdd3d7c8f0c231a8
hofmannnn <nadav.hofmann@tabit.cloud>
2021-11-04 12:30:23 +0200

    working example - using a set before refactor

--- cache.js
+++ cache.js
@@ -1,36 +1,79 @@
-/**
- * we should find item by index - fast..
- * keep the cache size limit
- */
-
 module.exports = Cache = class Cache {
     constructor(size) {
         this.size = size;
-        this.storage = {};
+        this.storage = new Set();
         // key => ref
         this.indices = {};
-        this.lastItemIndex = 0;
-    }
-
-    get(key) {
-        return this.indices[key];
-    }
-
-    put(key, value) {
-        this.setNext();
-        this.storage[this.lastItemIndex] = { [key]: value };
-        this.indices[key] = this.storage[this.lastItemIndex];
     }
 
     /**
-     * Return next available index;
+     * return the element or undefined if not found
+     * @param key
+     * @returns {*}
      */
-    setNext() {
-        if (this.lastItemIndex < this.size)
-            return this.lastItemIndex++;
+    get(key) {
+        // get the element and bump it..
+        if (this.indices[key]) {
+            this.storage.delete(key);
+            this.storage.add(key);
+            return this.indices[key];
+        }
 
-        // clean occupied index
-        delete this.indices[Object.keys(this.storage[this.lastItemIndex])];
-        return this.lastItemIndex;
+        return this.indices[key];
+    }
+
+    // add key
+    // key already exists? -> if so update
+    put(key, value) {
+        console.log('key**', key);
+        // check if already exists
+        let hasKeyExists = this.storage.has(key);
+        if (hasKeyExists) {
+            console.log('hasKeyExists**', hasKeyExists, 'key ', key);
+            // update the value in indices
+            this.indices[key] = { [key]: value };
+            // bump it on the set
+            console.log('key**', key);
+            this.storage.delete(key);
+            this.storage.add(key);
+            return;
+        }
+
+        // check size
+        if (this.storage.size === this.size) {
+            console.log('size..')
+            // remove the first inserted element
+            let firstInserted = this.storage.values().next();
+            console.log('firstInserted**', firstInserted);
+            console.log('size before delete**', this.storage.size);
+            this.storage.delete(firstInserted.value);
+
+            delete this.indices[firstInserted.value];
+            console.log('size after delete**', this.storage.size);
+            // add the new element
+            this.storage.add(key);
+            this.indices[key] = { [key]: value };
+            return;
+        }
+
+        this.storage.add(key);
+        this.indices[key] = { [key]: value };
     }
 }
+
+const cache = new Cache(2);
+// cache.put('nir', 'bla');
+// cache.put('nadav', 'bla');
+// cache.put('yaron', 'bla');
+// // expected: nadav, yaron
+//
+// console.log('cache**', JSON.stringify(cache, null, 2));
+
+
+cache.put('nir', 'bla');
+cache.put('nadav', 'bla');
+cache.get('nir', 'bla');
+cache.put('yaron', 'bla');
+// expected: nir, yaron
+
+console.log('cache**', JSON.stringify(cache, null, 2));
